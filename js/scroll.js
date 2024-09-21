1e672b4ed9668969cc43d4708f2209d229609809
thenamewaseat <82721790+thenamewaseat@users.noreply.github.com>
2024-09-22 00:27:21 +0800

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -3,7 +3,7 @@ let lenis;
     if (Webflow.env("editor") === undefined) {
     lenis = new Lenis({
         lerp: 0.12,
-        wheelMultiplier: 0.8,
+        wheelMultiplier: 0.9,
         gestureOrientation: "vertical",
         normalizeWheel: false,
         smoothTouch: false
@@ -14,3 +14,28 @@ let lenis;
     }
     requestAnimationFrame(raf);
     }
+
+    window.addEventListener('scroll', function() {
+        const container = document.getElementById('header');
+        if (window.scrollY > 0) {
+            container.classList.add('scrolled');
+        } else {
+            container.classList.remove('scrolled');
+        }
+    });
+
+    let lastScrollTop = 0;
+    const element = document.getElementById("header");
+
+    window.addEventListener("scroll", function() {
+        const currentScroll = window.scrollY || document.documentElement.scrollTop;
+
+        if (currentScroll > lastScrollTop && window.scrollY > 300) {
+            // Scrolling Down
+            element.style.transform = "translate(0,-88px)";
+        } else {
+            // Scrolling Up
+            element.style.transform = "translate(0,0px)";
+        }
+        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+    });
