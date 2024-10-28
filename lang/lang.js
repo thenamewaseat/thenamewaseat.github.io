56e6fd1e04d815405b9eef6a2ab0ec9e602835a9
thenamewaseat <82721790+thenamewaseat@users.noreply.github.com>
2024-10-29 00:48:41 +0800

    Add files via upload

---
+++ lang/lang.js
@@ -0,0 +1,93 @@
+var linguJSON = {
+    "languages": [
+        {
+            "lang_name": "English",
+            "lang_code": "en",
+            "url_pattern": "?"
+        },
+        {
+            "lang_name": "中文",
+            "lang_code": "zh",
+            "url_pattern": "?"
+        },
+        {
+            "lang_name": "日本語",
+            "lang_code": "ja",
+            "url_pattern": "?"
+        }
+    ],
+    "custom_lang_switcher_html": "<div id=\"langswitcher\" class=\"notranslate\"><a id=\"currlanglink\" href=\"\">[[linguCurrLang]]</a> <a href=\"[[linguTargetLangHref1]]\" class=\"targetlang\">[[linguTargetLang1]]</a><a href=\"[[linguTargetLangHref2]]\" class=\"targetlang\">[[linguTargetLang2]]</a></div>",
+    "custom_lang_switcher_container_id": "langlinks",
+    "translated_segments": [
+        {
+            "source": "About",
+            "target_zh": "關於",
+            "target_ja": "紹介"
+            
+        },
+        {
+            "source": "Gallery",
+            "target_zh": "作品集",
+            "target_ja": "ギャラリー"
+            
+        },
+        {
+            "source": "Blogs",
+            "target_zh": "部落格",
+            "target_ja": "ブログ"
+        },
+        {
+            "source": "Pricing",
+            "target_zh": "價錢",
+            "target_ja": "価格"
+        },
+        {
+            "source": "English",
+            "target_zh": "中文",
+            "target_ja": "日本語"
+        },
+        /*Index */
+        {
+            "source": "Thenamewaseat is a Student from Creative Media",
+            "target_ja": "Thenamewaseatはクリエイティブ・メディア学科の学生。"
+            
+        },
+        {
+            "source": "I am thenamewaseat, who are passionate in learning game related skills, such as Live 2D. I also dig in to installation, Game and App design and drawings.",
+            "target_ja": "thenamewaseatはLive 2Dなどゲーム関連のスキル習得に熱中している。インスタレーション、ゲームやアプリのデザイン、ドローイングも得意です。"
+            
+        },
+        /*About */
+        {
+            "source": "Skills",
+            "target_zh": "技能",
+            "target_ja": "技能"
+        },
+        {
+            "source": "<b>Software</b>",
+            "target_zh": "<b>軟體</b>",
+            "target_ja": "<b>ソフト</b>"
+        },
+        {
+            "source": "<b>Language</b>",
+            "target_zh": "<b>語言</b>",
+            "target_ja": "<b>言語</b>"
+        },
+        /*Gallery */
+        {
+            "source": "Works that I done before",
+            "target_zh": "過去的作品",
+            "target_ja": "過去に手がけた作品"
+            
+        },
+        {
+            "source": "(page not yet finished, please refer to pdf below)",
+            "target_ja": "(未完成のページがありますので、下記PDFをご参照ください。）"
+        }
+    ]/*,
+    "translated_pages": [
+      {
+          "slug": "index.html" //we only have translation for this Demo page; other links on the page (ie. link to www.lingumania.com homepage will not be prefixed with fr.lingumania.com or es.lingumania.com)
+      }
+    ]*/
+};
